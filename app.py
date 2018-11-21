18e423e1b3c62360d147fee35810a67f1c8abec0
Morgan Newman <mpnewman95@gmail.com>
2018-11-21 10:37:30 -0800

    Adds app-wide config

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 # Import Flask and deps
 from flask import Flask
 from flask import request
@@ -7,30 +8,32 @@ from flask_cors import CORS
 
 # Init app
 app = Flask(__name__)
+# Default to production environment for security
+app.config.from_object(os.getenv("APP_SETTINGS") or "config.ProductionConfig")
 
 # Set CORS permissions
 CORS(app, resources={r"/*": {"origins": "*"}}, send_wildcard=True)
 
 
 # POST /search endpoint
-@app.route('/search', methods=['POST'])
+@app.route("/search", methods=["POST"])
 def request_handler():
-    if request.method == 'POST':
-        data = (request.get_json())['content']
+    if request.method == "POST":
+        data = (request.get_json())["content"]
         # INSERT MAGIC FUNCTION TO DO MAGIC THINGS HERE
-        images = {'placeholder': 'text'}
+        images = {"placeholder": "text"}
         response = jsonify(images)
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add("Access-Control-Allow-Origin", "*")
         return response, 201
     else:
-        return jsonify({'Error': 'does not exist'}, 404)
+        return jsonify({"Error": "does not exist"}, 404)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Nothing to see here ;)'
+    return "Nothing to see here ;)"
 
 
 # Spin up server
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
