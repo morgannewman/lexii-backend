c9d74600b473bd4984a58f77b46c0708cc686854
Morgan Newman <mpnewman95@gmail.com>
2018-11-21 13:53:09 -0800

    Adds Snippet and User models

--- server/models.py
+++ server/models.py
@@ -13,3 +13,13 @@ class Snippet(db.Model):
 
     def __repr__(self):
         return '<content {}>'.format(self.content)
+
+
+class User(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
--- server/routes.py
+++ server/routes.py
@@ -5,13 +5,3 @@ from server import app, db
 @app.route('/index')
 def index():
     return "Hello, World!"
-
-
-class User(db.Model):
-    """ User Model for storing user related details """
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
