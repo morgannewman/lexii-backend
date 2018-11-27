b765c60d45db10a6e7992cd347258fb592065fa9
Morgan Newman <mpnewman95@gmail.com>
2018-11-26 19:31:42 -0800

    Updates Config to use env variables

--- server/config.py
+++ server/config.py
@@ -6,10 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY") or "NOT_A_REAL_SECRET"
     DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql://dev:@localhost/lexii"
-    UNSPLASH_CLIENT_ID = (
-        "38d91223e00315d4c6926c55580e4e8c2ea513672893c1bdda4b3cbca4a09f31"
-    )
-    UNSPLASH_CLIENT_SECRET = (
-        "64efe5a0ee03d7bb161c111c6fc7e18b5d73e184d7ffe4294f09b3dbc099374b"
-    )
-
+    UNSPLASH_CLIENT_ID = os.getenv("UNSPLASH_CLIENT_ID")
+    GOOGLE_CLOUD_API_KEY = os.getenv("GOOGLE_CLOUD_API_KEY")
